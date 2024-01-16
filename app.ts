9bd724395f7faa9b52e78e94e640443af50e3227
Esmat Najafi <145345735+Esmatnajafi@users.noreply.github.com>
2024-01-16 14:07:39 +0100

    three objects

--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
 let animals: string [] = ["hund", "orangutang", "gorilla", "cat", "giraff"];
 console.log(animals);
 
+let threeObjects = {
+    str: "hello World",
+    num: 65,
+    bool: false
+};
+
+console.log(threeObjects);
 
